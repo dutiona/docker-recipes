19ea802dc89fe2bd2f3e55d46f3a2554a97ba609
Michaël Roynard <michaelroynard@gmail.com>
2018-02-05 17:12:29 +0100

    Fix command line argument parsing

--- mroynard/ubuntu-toolset/build-in-docker.sh
+++ mroynard/ubuntu-toolset/build-in-docker.sh
@@ -8,37 +8,33 @@ SOURCE_DIR=".."
 BUILD_COMPILER="gcc"
 BUILD_TARGET="all"
 BUILD_TYPE="Debug"
-BUILD_CMAKE_ARGUMENTS=""
-BUILD_TOOLCHAIN_ARGUMENTS=""
 FORCE=""
 VERBOSE=""
+UNDERLYING_ARGS=""
 USAGE="$(basename "$0") [OPTIONS] -- execute a build toolchain
 
 where:
     -h --help                       show this help text
     -c --compiler gcc|clang         select the compiler to use
                                     default = gcc
-    -g --cmake-generator Gen        use provided cmake generator
+    -g --cmake-generator Generator  use provided cmake generator
                                     default = Unix Makefiles
-    -d --build-directory Dir        use provided build directory for build artifacts (on host)
+    -d --build-directory Directory  use provided build directory for build artifacts (on host)
                                     default = build-in-docker
-    -s --source-directory Dir       use provided source directory to compile
+    -s --source-directory Directory use provided source directory to compile
                                     default = ..
-    -t --target                     build target passed to the generated toolchain (make target)
+    -t --target Target              build target passed to the generated toolchain (make target)
                                     default = all
+    -r --release-type ReleaseType             build type. Release|Debug|RelWithDebInfo|MinSizeRel
+                                    default = Debug
+
     -v --verbose                    if passed, enable verbose to underlying commands
 
-    -r --release-type               build type. Release|Debug|RelWithDebInfo|MinSizeRel
-                                    default = Debug
-    -f --force                      mpty build directory to force a full rebuild
+    -f --force                      empty build directory to force a full rebuild
     
-    --cmake-arguments=(args...)     arguments to pass to cmake (-D...)
-
-    --toolchain-arguments=(args...) argumennts to pass to underlying toolchain (passed after --)
-                                    passing -j4 here will result in cmake --build ... -- -j4
+    --                              end of arguments for script, pass the rest to cmake
     "
 
-POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -46,6 +42,14 @@ case $key in
     -h|--help)
     echo "$USAGE"
     exit
+    ;;
+    -f|--force)
+    FORCE="--force"
+    shift
+    ;;
+    -v|--verbose)
+    VERBOSE="--verbose"
+    shift
     ;;
     -c|--compiler)
     BUILD_COMPILER="$2"
@@ -71,44 +75,27 @@ case $key in
     BUILD_TYPE="$2"
     shift 2
     ;;
-    -f|--force)
-    FORCE="--force"
+    --)
     shift
-    ;;
-    -v|--verbose)
-    VERBOSE="--verbose"
-    shift
-    ;;
-    --cmake-arguments=*)
-    BUILD_CMAKE_ARGUMENTS="${i#*=}"
-    shift 2
-    ;;
-    --toolchain-arguments=*)
-    BUILD_TOOLCHAIN_ARGUMENTS="${i#*=}"
-    shift 2
     ;;
     *)
-    POSITIONAL+=("$1")
+    UNDERLYING_ARGS="$UNDERLYING_ARGS $1"
     shift
     ;;
 esac
 done
-set -- "${POSITIONAL[@]}" # restore positional parameters
 
 docker run --rm \
     -it \
     --name docker-builder \
     --mount type=bind,source="$(pwd)",target=/workspace \
     mroynard/ubuntu-toolset:latest \
-    build-dispatch \
-        -c "$BUILD_COMPILER" \
-        -g "$BUILD_GENERATOR" \
-        -d "$BUILD_DIR" \
-        -s "$SOURCE_DIR" \
-        -t "$BUILD_TARGET" \
-        -r "$BUILD_TYPE" \
         $VERBOSE \
         $FORCE \
-        --cmake-arguments="$BUILD_CMAKE_ARGUMENTS" \
-        --toolchain-arguments="$BUILD_TOOLCHAIN_ARGUMENTS"
-        #--cmake-arguments=-DWITH_EXAMPLES=ON -DWITH_BENCHMARK=ON
+        -c $BUILD_COMPILER \
+        -g $BUILD_GENERATOR \
+        -d $BUILD_DIR \
+        -s $SOURCE_DIR \
+        -t $BUILD_TARGET \
+        -r $BUILD_TYPE \
+        -- $UNDERLYING_ARGS
